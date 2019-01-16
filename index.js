4301cb5dfc732caa6830628c1e88687166ca2177
Jehy <fate@jehy.ru>
2019-01-16 19:16:40 +0300

    debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports.rules = {
   "no-log-without-message": context =>
     ({ MemberExpression: function(node) {
         if (node.object.name === "log" && node.property.name === "e") {
-          context.report(node, `DEBUG: ${JSON.stringify(node)}`);
+          context.report(node, `DEBUG: ${node.parent.arguments[0].type} ${node.parent.arguments[0].value}`);
         }
       }
     }),
